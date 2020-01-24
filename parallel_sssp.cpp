bc40d749388b7cd3c3be0a928be5768c1589dc1f
7vikpeculiar <bugganasathviksanjeev1998@gmail.com>
2020-01-24 13:47:35 +0530

    Properly done

--- parallel_sssp.cpp
+++ parallel_sssp.cpp
@@ -6,80 +6,43 @@
 #include <stdlib.h>
 #include <queue>
 #include <limits>
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
 
 using namespace std;
 
-struct distanceNode
+void min_array(int *dist1, int *dist2, int n)
 {
-    int vertex;
-    int distance;
-    int proc;
-};
-
-distanceNode min(distanceNode a, distanceNode b)
-{
-    if (a.distance <= b.distance)
+    for (int i = 0; i < n; i++)
     {
-        return a;
-    }
-    else
-    {
-        return b;
+        dist1[i] = min(dist1[i], dist2[i]);
     }
 }
-
-int minNode(int nporcs, int *allVertices)
-{
-    int vertex = INT_MAX;
-    int dist = INT_MAX;
-    for (int i = 0; i < nporcs; i++)
-    {
-        if (allVertices[i] < dist)
-        {
-            dist = allVertices[i];
-            vertex = i;
-        }
-    }
-    return vertex;
-}
-
-int minDistance(int *dist, bool *visited, int V)
-{
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < V; v++)
-        if (visited[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-
-    return min_index;
-}
-
-// int getMinVertex(int **A, int col_size, int minVertex)
-// {
-//     for (int i = 0; i < col_size; i++)
-//     {
-//         A[i][minVertex]
-//     }
-// }
 
 int main(int argc, char **argv)
 {
 
-    int A[4][4] = {
-        {0, 1, 1, 3},
-        {1, 0, 1, 1},
-        {1, 1, 0, 0},
-        {3, 1, 0, 0}};
+    // int A[10][3] = {
+    //     {0, 1, 1},
+    //     {0, 2, 1},
+    //     {0, 3, 3},
+    //     {1, 3, 1},
+    //     {2, 3, 1},
+
+    //     {1, 0, 1},
+    //     {2, 0, 1},
+    //     {3, 0, 3},
+    //     {3, 1, 1},
+    //     {3, 2, 1}};
+    int A[][3] = {{0, 1, -1}, {0, 2, 4}, {1, 2, 3}, {1, 3, 2}, {1, 4, 2}, {3, 2, 5}, {3, 1, 1}, {4, 3, -3}};
     MPI_Status status;
-    // 7 -1 4 9 -8 0 6 1
     int rank, numprocs;
-    int n;
-    int m;
-    int col_size;
+    int vertices;
+    int edges;
+    int *dist_array_copy;
+    int block_size;
     int src_vertex;
-    int minVertex;
-    int starting_vertex;
+    int other_block_size;
+    // int minVertex;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -88,45 +51,40 @@ int main(int argc, char **argv)
 
     if (rank == 0)
     {
-        n = 4;
-        m = 5;
-        col_size = n / numprocs;
+        vertices = 5;
+        edges = 8;
+        block_size = other_block_size = edges / numprocs;
         src_vertex = 0;
     }
-    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&col_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    int dist[n];
-    bool visited[n];
-
-    // cout << "Col_Size " << col_size << " ";
+    MPI_Bcast(&vertices, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&edges, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&block_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
     if (rank == 0)
     {
+        block_size = edges / numprocs + edges % numprocs;
+    }
+    int dist[block_size];
 
-        // cout << col_size << " ";
-        int procId, startingVertexForProc;
+    if (rank == 0)
+    {
+        int procId;
         for (procId = 1; procId < numprocs; procId++)
         {
-            cout << rank << " " << col_size << " "
-                 << "<-\n ";
-            MPI_Send(&A[col_size * procId][0], col_size * n, MPI_INT, procId, 1, MPI_COMM_WORLD);
-            startingVertexForProc = procId * col_size;
-            MPI_Send(&startingVertexForProc, 1, MPI_INT, procId, 1, MPI_COMM_WORLD);
+
+            MPI_Send(&A[block_size + other_block_size * (procId - 1)][0], other_block_size * 3, MPI_INT, procId, 1, MPI_COMM_WORLD);
         }
     }
     else
     {
-        cout << rank << " " << col_size << " "
-             << "<-\n ";
 
-        MPI_Recv(&A[0][0], col_size * n, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&starting_vertex, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&A[0][0], block_size * 3, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        // MPI_Recv(&starting_vertex, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
     }
     cout << "======================\n";
-    for (int i = 0; i < col_size; i++)
+    for (int i = 0; i < block_size; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < 3; j++)
         {
             cout << A[i][j] << " ";
         }
@@ -137,46 +95,68 @@ int main(int argc, char **argv)
     // Got the pieces and now source vertex also
     MPI_Bcast(&src_vertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    for (int i = 0; i < col_size; i++)
+    // Initialize
+    for (int i = 0; i < vertices; i++)
     {
         dist[i] = INT_MAX;
-        visited[i] = false;
+    }
+
+    dist[src_vertex] = 0;
+
+    // cout << "Rank : " << rank << " dist : ";
+    //     for (int i = 0; i < vertices; i++)
+    //     {
+    //         cout << dist[i] << " ";
+    //     }
+    //     cout << endl;
+
+    if (rank == 0)
+    {
+        dist_array_copy = (int *)malloc(vertices * sizeof(int));
+    }
+
+    for (int i = 0; i < vertices; i++)
+    {
+        for (int j = 0; j < block_size; j++)
+        {
+            if (dist[A[j][0]] != INT_MAX && dist[A[j][0]] + A[j][2] <
+                                                dist[A[j][1]])
+            {
+                dist[A[j][1]] =
+                    dist[A[j][0]] + A[j][2];
+            }
+        }
+        if (rank != 0)
+        {
+            MPI_Send(&dist[0], vertices, MPI_INT, 0, 3, MPI_COMM_WORLD);
+        }
+        else
+        {
+
+            for (int i = 1; i < numprocs; i++)
+            {
+                MPI_Recv(&dist_array_copy[0], vertices, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
+                // for (int i = 0; i < vertices; i++)
+                // {
+                //     cout << dist_array_copy[i] << " ";
+                // }
+                // cout << endl;
+
+                min_array(dist, dist_array_copy, vertices);
+            }
+        }
+        MPI_Bcast(&dist[0], vertices, MPI_INT, 0, MPI_COMM_WORLD);
+        // break;
     }
 
     if (rank == 0)
     {
-        dist[src_vertex] = 0;
-    }
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (rank == 0)
+        for (int i = 0; i < vertices; i++)
         {
-            // n iterations lo, SSP has to be done
-            minVertex = minDistance(dist, visited, col_size);
-            distanceNode a = {minVertex, dist[minVertex], 0};
-            for (int procId = 1; procId < numprocs; procId++)
-            {
-                distanceNode b;
-                MPI_Recv(&b, 3, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
-                a = min(a, b);
-            }
-            cout << "Node :: " << a.distance << " " << a.proc << " " << a.vertex << endl;
-            // MPI_Bcast(&minVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            // int minVal = getMinVertex(A, col_size, minVertex);
+            cout << dist[i] << " ";
         }
-        else
-        {
-            // Got min vertex
-            minVertex = minDistance(dist, visited, col_size);
-            distanceNode a = {minVertex, dist[minVertex], 0};
-            for (int procId = 1; procId < numprocs; procId++)
-            {
-                MPI_Send(&a, 3, MPI_INT, 0, 1, MPI_COMM_WORLD);
-            }
-            // MPI_Bcast(&minVertex, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            // int minVal = getMinVertex(A, col_size, minVertex);
-        }
+        cout << endl;
+        // break;
     }
 
     MPI_Barrier(MPI_COMM_WORLD);
--- tempe.c
+++ tempe.c
@@ -1,91 +1,89 @@
 
-// A C++ program for Dijkstra's single source shortest path algorithm. 
-// The program is for adjacency matrix representation of the graph 
-  
-#include <limits.h> 
-#include <stdio.h> 
-  
-// Number of vertices in the graph 
-#define V 9 
-  
-// A utility function to find the vertex with minimum distance value, from 
-// the set of vertices not yet included in shortest path tree 
-int minDistance(int dist[], bool sptSet[]) 
-{ 
-    // Initialize min value 
-    int min = INT_MAX, min_index; 
-  
-    for (int v = 0; v < V; v++) 
-        if (sptSet[v] == false && dist[v] <= min) 
-            min = dist[v], min_index = v; 
-  
-    return min_index; 
-} 
-  
-// A utility function to print the constructed distance array 
-int printSolution(int dist[]) 
-{ 
-    printf("Vertex \t\t Distance from Source\n"); 
-    for (int i = 0; i < V; i++) 
-        printf("%d \t\t %d\n", i, dist[i]); 
-} 
-  
-// Function that implements Dijkstra's single source shortest path algorithm 
-// for a graph represented using adjacency matrix representation 
-void dijkstra(int graph[V][V], int src) 
-{ 
-    int dist[V]; // The output array.  dist[i] will hold the shortest 
-    // distance from src to i 
-  
-    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest 
-    // path tree or shortest distance from src to i is finalized 
-  
-    // Initialize all distances as INFINITE and stpSet[] as false 
-    for (int i = 0; i < V; i++) 
-        dist[i] = INT_MAX, sptSet[i] = false; 
-  
-    // Distance of source vertex from itself is always 0 
-    dist[src] = 0; 
-  
-    // Find shortest path for all vertices 
-    for (int count = 0; count < V - 1; count++) { 
-        // Pick the minimum distance vertex from the set of vertices not 
-        // yet processed. u is always equal to src in the first iteration. 
-        int u = minDistance(dist, sptSet); 
-  
-        // Mark the picked vertex as processed 
-        sptSet[u] = true; 
-  
-        // Update dist value of the adjacent vertices of the picked vertex. 
-        for (int v = 0; v < V; v++) 
-  
-            // Update dist[v] only if is not in sptSet, there is an edge from 
-            // u to v, and total weight of path from src to  v through u is 
-            // smaller than current value of dist[v] 
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX 
-                && dist[u] + graph[u][v] < dist[v]) 
-                dist[v] = dist[u] + graph[u][v]; 
-    } 
-  
-    // print the constructed distance array 
-    printSolution(dist); 
-} 
-  
-// driver program to test above function 
-int main() 
-{ 
-    /* Let us create the example graph discussed above */
-    int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
-                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 }, 
-                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
-                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
-                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 }, 
-                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 }, 
-                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
-                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 }, 
-                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } }; 
-  
-    dijkstra(graph, 0); 
-  
-    return 0; 
-} 
+// A C++ program for Bellman-Ford's single source
+// shortest path algorithm.
+#include <bits/stdc++.h>
+using namespace std;
+// The main function that finds shortest
+// distances from src to all other vertices
+// using Bellman-Ford algorithm. The function
+// also detects negative weight cycle
+// The row graph[i] represents i-th edge with
+// three values u, v and w.
+void BellmanFord(int graph[][3], int V, int E,
+                 int src)
+{
+    // Initialize distance of all vertices as 0.
+    int dis[V];
+    for (int i = 0; i < V; i++)
+        dis[i] = INT_MAX;
+
+    // initialize distance of source as 0
+    dis[src] = 0;
+
+    // Relax all edges |V| - 1 times. A simple
+    // shortest path from src to any other
+    // vertex can have at-most |V| - 1 edges
+    for (int i = 0; i < V - 1; i++)
+    {
+
+        for (int j = 0; j < E; j++)
+        {
+            if (dis[graph[j][0]] + graph[j][2] <
+                dis[graph[j][1]])
+                dis[graph[j][1]] =
+                    dis[graph[j][0]] + graph[j][2];
+        }
+        for (int i = 0; i < V; i++)
+        {
+            printf("%d ",dis[i]);
+        }
+        printf("\n");
+    }
+
+    // check for negative-weight cycles.
+    // The above step guarantees shortest
+    // distances if graph doesn't contain
+    // negative weight cycle.  If we get a
+    // shorter path, then there is a cycle.
+    for (int i = 0; i < E; i++)
+    {
+        int x = graph[i][0];
+        int y = graph[i][1];
+        int weight = graph[i][2];
+        if (dis[x] != INT_MAX &&
+            dis[x] + weight < dis[y])
+            cout << "Graph contains negative"
+                    " weight cycle"
+                 << endl;
+    }
+
+    cout << "Vertex Distance from Source" << endl;
+    for (int i = 0; i < V; i++)
+        cout << i << "\t\t" << dis[i] << endl;
+}
+
+// Driver program to test above functions
+int main()
+{
+    int V = 5; // Number of vertices in graph
+    int E = 8; // Number of edges in graph
+
+    // Every edge has three values (u, v, w) where
+    // the edge is from vertex u to v. And weight
+    // of the edge is w.
+    // int graph[][3] = {{0, 1, -1}, {0, 2, 4}, {1, 2, 3}, {1, 3, 2}, {1, 4, 2}, {3, 2, 5}, {3, 1, 1}, {4, 3, -3}};
+    int graph[10][3] = {
+        {0, 1, 1},
+        {0, 2, 1},
+        {0, 3, 3},
+        {1, 3, 1},
+        {2, 3, 1},
+
+        {1, 0, 1},
+        {2, 0, 1},
+        {3, 0, 3},
+        {3, 1, 1},
+        {3, 2, 1}};
+    BellmanFord(graph, V, E, 0);
+    return 0;
+}
